55198662b8980042c170d970bee9360a0961e60d
VeeNeil <80906756+VeeNeil@users.noreply.github.com>
2023-05-08 22:30:08 +0700

    5/8/2023

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import { Sidebar } from './components/Sidebar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css"
+function App() {
+  return (
+    <div className= "App">
+      <Sidebar/>
+      <Banner/>
+      <Skills/>
+      <Projects/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+
+import LogoTitle from '../../assets/images/logo-s.png';
+const Home = () => {
+    return (
+        <div className="container home-page">
+           
+        </div>
+    );
+}
+
+export default Home;
